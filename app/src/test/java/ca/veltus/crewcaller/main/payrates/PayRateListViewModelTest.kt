e3de2b7dfaca3fb968eb83a30035bcffffbd865e
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-09-26 23:25:48 -0700

    test: Add test for loadPayRates function in PayRateListViewModelTest

--- app/src/test/java/ca/veltus/crewcaller/main/payrates/PayRateListViewModelTest.kt
+++ app/src/test/java/ca/veltus/crewcaller/main/payrates/PayRateListViewModelTest.kt
@@ -58,4 +58,64 @@ class PayRateListViewModelTest {
         }
     }
 
+    @Test
+    fun savePayRate_validValues_payRateAddedToDatabase() {
+        payRateListViewModel.payRateTier.value = "tier"
+        payRateListViewModel.payRatePosition.value = "position"
+        payRateListViewModel.payRateRate.value = "pay rate"
+        payRateListViewModel.payRateId.value = "id"
+
+        runBlocking { payRateListViewModel.savePayRate()
+
+            val result = dataSource.payRates!!.size
+
+            assertThat(result, IsEqual(1))
+        }
+    }
+
+    @Test
+    fun savePayRate_invalidTier_snackBarValueSet() {
+        payRateListViewModel.payRateTier.value = ""
+        payRateListViewModel.payRatePosition.value = "position"
+        payRateListViewModel.payRateRate.value = "pay rate"
+        payRateListViewModel.payRateId.value = "id"
+
+        runBlocking { payRateListViewModel.savePayRate()
+
+            val result = payRateListViewModel.showSnackBar.value
+
+            assertThat(result, IsEqual("Please Select a Tier"))
+        }
+    }
+
+    @Test
+    fun savePayRate_invalidPosition_snackBarValueSet() {
+        payRateListViewModel.payRateTier.value = "tier"
+        payRateListViewModel.payRatePosition.value = ""
+        payRateListViewModel.payRateRate.value = "pay rate"
+        payRateListViewModel.payRateId.value = "id"
+
+        runBlocking { payRateListViewModel.savePayRate()
+
+            val result = payRateListViewModel.showSnackBar.value
+
+            assertThat(result, IsEqual("Please Enter Your Position"))
+        }
+    }
+
+    @Test
+    fun savePayRate_invalidRate_snackBarValueSet() {
+        payRateListViewModel.payRateTier.value = "tier"
+        payRateListViewModel.payRatePosition.value = "position"
+        payRateListViewModel.payRateRate.value = ""
+        payRateListViewModel.payRateId.value = "id"
+
+        runBlocking { payRateListViewModel.savePayRate()
+
+            val result = payRateListViewModel.showSnackBar.value
+
+            assertThat(result, IsEqual("Please Enter Your Rate"))
+        }
+    }
+
 }
