87c114b0764fcacf1a691bc31a776308912970e6
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2022-09-28 20:02:02 -0700

    test: Add test class for CalendarViewModel

---
+++ app/src/test/java/ca/veltus/crewcaller/main/calendar/CalendarViewModelTest.kt
@@ -0,0 +1,47 @@
+package ca.veltus.crewcaller.main.calendar
+
+import android.app.Application
+import android.os.Build
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import ca.veltus.crewcaller.MainCoroutineRule
+import ca.veltus.crewcaller.main.contacts.ContactListViewModel
+import ca.veltus.crewcaller.main.data.FakeCrewCallerDataSource
+import com.google.firebase.FirebaseApp
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
+import org.robolectric.annotation.Config
+
+@ExperimentalCoroutinesApi
+@Config(sdk = [Build.VERSION_CODES.P])
+@RunWith(AndroidJUnit4::class)
+class CalendarViewModelTest {
+
+    private lateinit var calendarViewModel: CalendarViewModel
+    private val dataSource = FakeCrewCallerDataSource()
+    private lateinit var app: Application
+
+    // Set the main coroutines dispatcher for unit testing.
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+
+    @Before
+    fun initSetup() {
+        stopKoin()
+        app = ApplicationProvider.getApplicationContext()
+        FirebaseApp.initializeApp(app)
+        calendarViewModel = CalendarViewModel(app, dataSource)
+    }
+
+}
